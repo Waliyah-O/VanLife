4b54b119dd65b026f6daa95945f2f790d3580ed0
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-07 09:16:58 +0100

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
+  const activeStyle = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
   return (
     <div>
       <header>
@@ -8,9 +13,24 @@ const Header = () => {
           #VANLIFE
         </Link>
         <nav>
-          <Link to="/host">Host</Link>
-          <Link to="/about">About page</Link>
-          <Link to="/vans">Vans</Link>
+          <NavLink
+            to="/host"
+            style={({ isActive }) => (isActive ? activeStyle : null)}
+          >
+            Host
+          </NavLink>
+          <NavLink
+            to="/about"
+            style={({ isActive }) => (isActive ? activeStyle : null)}
+          >
+            About page
+          </NavLink>
+          <NavLink
+            to="/vans"
+            style={({ isActive }) => (isActive ? activeStyle : null)}
+          >
+            Vans
+          </NavLink>
         </nav>
       </header>
     </div>
