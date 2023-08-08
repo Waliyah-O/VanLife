239b25267bd60dd12b616edf38cea8c2739c0c04
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-08 16:16:46 +0100

    building the host van detail page

--- src/pages/Host/HostVanDetails.jsx
+++ src/pages/Host/HostVanDetails.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const HostVanDetails = () => {
+  const [currentVan, setCurrentVan] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans))
+      .catch((error) => console.log(error));
+  }, [id]);
+
+  if (!currentVan) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <div>
       <h1>Host Van Details</h1>
+      <div>
+        <img src={currentVan.imageUrl} alt="" />
+        <h2>{currentVan.name}</h2>
+        <p>${currentVan.price}/day</p>
+        <p>{currentVan.type}</p>
+      </div>
     </div>
   );
 };
