bfac4c79edd93ef3c0b62bbb57269a84f5e8babe
Waliyah Okunade <waliyahiacademy@gmail.com>
2023-08-25 09:27:06 +0100

    tutu

--- src/pages/Host/HostVanPhotos.jsx
+++ src/pages/Host/HostVanPhotos.jsx
@@ -1,29 +1,6 @@
-import { useOutletContext, useSearchParams } from "react-router-dom";
-
-const swCharacters = [
-  { name: "Luke Skywalker", type: "Jedi" },
-  { name: "Darth Vader", type: "Sith" },
-  { name: "Emperor Palpatine", type: "Sith" },
-  { name: "Yoda", type: "Jedi" },
-];
+import { useOutletContext } from "react-router-dom";
 
 const HostVanPhotos = () => {
-  const [searchparams, setSearchParams] = useSearchParams();
-
-  console.log(searchparams.toString());
-
-  const charEls = swCharacters.map((char) => (
-    <div key={char.name}>
-      <h3
-        style={{ color: char.type.toLowerCase() === "jedi" ? "blue" : "red" }}
-      >
-        Name: {char.name}
-      </h3>
-      <p>Type: {char.type}</p>
-      <hr />
-    </div>
-  ));
-
   const { currentVan } = useOutletContext();
 
   return (
@@ -33,8 +10,6 @@ const HostVanPhotos = () => {
         src={currentVan.imageUrl}
         alt={`Photo of ${currentVan.name}`}
       />
-
-      <div>{charEls}</div>
     </div>
   );
 };
